d206bba76010d2dd431db5c81cf55f728ed17c55
Akhsanul Fikri Nur Fauzan <akhsanulfikrinurfauzan@gmail.com>
2024-08-03 19:41:52 +0700

    Rename title
    Just rename the title.

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "UTS Pemrograman Lanjut",
+  title: "LinkBooks",
   description: "By Akhsanul Fikri Nur Fauzan",
 };
 
